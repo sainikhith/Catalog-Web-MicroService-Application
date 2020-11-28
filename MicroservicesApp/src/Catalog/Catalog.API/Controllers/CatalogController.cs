35fd705e8e13c9aee3dd33b6109a916457679fa2
Sai Nikhith <sainikhith98v@gmail.com>
2020-11-28 22:28:20 +0530

    Commit First

---
+++ MicroservicesApp/src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,102 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/nik/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        #region Private Fields
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+        #endregion
+
+        #region Constructor
+        public CatalogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        #endregion
+
+        #region GetList
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+        #endregion
+
+        #region GetProductById
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+            if(product == null)
+            {
+                _logger.LogError($"Product with id: {id} ,not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        #endregion
+
+        #region GetCategoryByName
+        [Route("[action]/{category}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(Product),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductByCategory(string category)
+        {
+            var product = await _productRepository.GetProductByCategory(category);
+            if (product == null)
+            {
+                _logger.LogError($"Product with Category Name: {category} ,not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        #endregion
+
+        #region Create Product
+        [HttpPost]
+        [ProducesResponseType(typeof(Product),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.Create(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+        #endregion
+
+        #region Update Product
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.Update(product));
+        }
+        #endregion
+
+        #region Delete Product
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            return Ok(await _productRepository.Delete(id));
+        }
+        #endregion
+
+
+    }
+}
---
+++ MicroservicesApp/src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            #region If there is no data in the database data is seeded on program startup
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(getPreConfiguredProduct());
+            }
+            #endregion
+        }
+
+        private static IEnumerable<Product> getPreConfiguredProduct()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone SE",
+                    Summary = "asd",
+                    Description = "bsd",
+                    Category = "Cata",
+                    ImageFile = "p1.jpg",
+                    Price = 54.93M
+                },
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "asd",
+                    Description = "bsd",
+                    Category = "Cata",
+                    ImageFile = "p1.jpg",
+                    Price = 984.93M
+                },
+                new Product()
+                {
+                    Name = "Iphone 12",
+                    Summary = "asd",
+                    Description = "bsd",
+                    Category = "Cata",
+                    ImageFile = "p1.jpg",
+                    Price = 1006.93M
+                },
+                new Product()
+                {
+                    Name = "Ipone 6s",
+                    Summary = "asd",
+                    Description = "bsd",
+                    Category = "Cata",
+                    ImageFile = "p1.jpg",
+                    Price = 54.93M
+                }
+        };
+        }
+    }
+}
