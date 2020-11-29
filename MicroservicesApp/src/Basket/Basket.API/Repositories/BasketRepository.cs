33211d3c4b03d359a8b7b3cb8ec2d8fe7910b9cf
Sai Nikhith <sainikhith98v@gmail.com>
2020-11-29 18:34:15 +0530

    Committing-Repository-layer-Basket

---
+++ MicroservicesApp/src/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,53 @@
+﻿using Basket.API.Data.Interfaces;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        #region Private Variables
+        private readonly IBasketContext _context;
+        #endregion
+
+        #region Constructor
+        public BasketRepository(IBasketContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+        public async Task<BasketCart> GetBasket(string userName)
+        {
+            var basket = await _context.Redis.StringGetAsync(userName);
+            if (basket.IsNullOrEmpty)
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<BasketCart>(basket);
+        }
+
+        public async Task<BasketCart> UpdateBasket(BasketCart basket)
+        {
+            var updated = await _context.Redis.StringSetAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            if (!updated)
+            {
+                return null;
+            }
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task<bool> DeleteBasket(string userName)
+        {
+            return await _context.Redis.KeyDeleteAsync(userName);
+        }
+
+       
+    }
+}
